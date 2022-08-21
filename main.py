fa0d0b2003a26af8b2472732ebb9964fa9dbcd86
P-AP-NDA <swirlyero2@gmail.com>
2022-08-21 19:18:06 -0400

    saddsadasdasd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import pygame
-
+import time
 import random
 
 pygame.init()
@@ -15,28 +15,40 @@ y2 = 100
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-width = 25
-height = 25
+width = 15
+height = 15
 
 x1_change = 0
 y1_change = 0
 
-snake_block = 10
+snake_block = 15
+
+Score = 0
 
 length_snake = 1
 snake_List = []
 
 def snake(snake_piece, snake_List):
-    for x in snake_List:
+     for x in snake_List:
         pygame.draw.rect(win, [255, 0, 0], [x[0], x[1], snake_block, snake_block])
 
-Score = 0
+
+running = True
+game_over = False
+game_close = False
 
 clock = pygame.time.Clock()
 
-running = True
 
 while running:
+
+    '''while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    game_close = True
+                    game_over = False
+                    '''
 
     pygame.time.delay(10)
 
@@ -61,16 +73,16 @@ while running:
 
     x1 += x1_change
     y1 += y1_change
+    win.fill(black)
     win_surface = pygame.display.set_mode((500, 500))
     font = pygame.font.Font('freesansbold.ttf', 20)
     scoretxt = font.render("Score: " + str(Score), True, white)
-    win.fill(white)
 
     textRect = scoretxt.get_rect()
     textRect.center = (50, 30)
 
     player = pygame.draw.rect(win, (255, 0 ,0), [x1, y1, width, height])
-    apple = pygame.draw.rect(win, (55, 89, 255), [x2, y2, width, height])
+    apple = pygame.draw.rect(win, (55, 89, 255), [x2, y2, width + 10, height + 10])
     snake_piece = []
     snake_piece.append(x1)
     snake_piece.append(y1)
@@ -79,11 +91,8 @@ while running:
     if len(snake_List) > length_snake:
         del snake_List[0]
 
-    for x in snake_List[:-1]:
-        if x == snake_piece:
-            pygame.quit()
-
     snake(snake_block, snake_List)
+
 
     #Text
     win_surface.blit(scoretxt, textRect)
@@ -96,7 +105,7 @@ while running:
     # Score
     if player.colliderect(apple):
         Score += 1
-        length_snake += 1
+        length_snake += 5
 
 
     #Border detection
@@ -113,9 +122,8 @@ while running:
         pygame.QUIT()
         quit()
 
-
-
     pygame.display.update()
+
 
     clock.tick(30)
 
